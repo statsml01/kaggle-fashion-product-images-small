2f5158e15a7dd9d7e5ec0e027da984b216e360fe
statsml01 <statsml01@gmail.com>
2025-01-04 23:37:41 +0900

    Update train.py

--- train.py
+++ train.py
@@ -3,7 +3,7 @@ import os
 import torch
 import torch.nn as nn
 import torch.optim as optim
-from torch.utils.data import DataLoader
+from torch.utils.data import Dataset, DataLoader
 from tqdm import tqdm
 import pandas as pd
 
